359ecf0ab3fb582eda0542fe60aa4254a6245428
amirkenesbaylearn <amirkenesbay7@gmail.com>
2022-09-13 18:45:51 +0600

    Через VUE.js создал функции которые добавляет, изменяет и удаляет элементы в списке

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,28 @@
+// https://www.youtube.com/watch?v=E0S1ZrQP0-Y&list=PLU2ftbIeotGqSTOVNjT4L3Yfy8jatCdhm&index=3
+// 11:52
+function getIndex(list, id) {
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].id === id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 var messageApi = Vue.resource('/message{/id}');
 
 Vue.component('message-form', {
-    props: ['messages'],
+    props: ['messages', 'messageAttr'],
     data: function () {
         return {
-            text: ''
+            text: '',
+            id: ''
+        }
+    },
+    watch: {
+        messageAttr: function (newVal, oldVal) {
+            this.text = newVal.text
+            this.id = newVal.id
         }
     },
     template:
@@ -14,28 +32,61 @@ Vue.component('message-form', {
         '<div>',
     methods: {
         save: function () {
-            var message = { text: this.text };
-            messageApi.save({}, message).then(result =>
-                result.json().then(data => {
-                    this.messages.push(data);
-                    this.text = ''
-                })
-            )
+            var message = {text: this.text};
+            if (this.id) {
+                messageApi.update({id: this.id}, message).then(result => result.json().then(data => {
+                    var index = getIndex(this.messages, data.id);
+                    this.messages.splice(index, 1, data);
+                }))
+            } else {
+                messageApi.save({}, message).then(result =>
+                    result.json().then(data => {
+                        this.messages.push(data);
+                        this.text = ''
+                        this.id = ''
+                    })
+                )
+            }
         }
     }
 });
 
 Vue.component('message-row', {
-    props: ['message'],
-    template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>'
+    props: ['message', 'editMethod', 'messages'],
+    template:
+        '<div>' +
+        '<i>({{ message.id }})</i> {{ message.text }}' +
+        '<span style="position: absolute; right: 0">' +
+            '<input type="button" value="Edit" @click="edit" />' +
+            '<input type="button" value="X" @click="del" />' +
+        '</span>' +
+        '</div>',
+    methods: {
+        edit: function () {
+            this.editMethod(this.message);
+        },
+        del: function (){
+            messageApi.remove({id: this.message.id}).then(result => {
+                if(result.ok){
+                    this.message.splice(this.messages.indexOf(this.message), 1)
+                }
+            });
+        }
+    }
 });
 
 Vue.component('messages-list', {
     props: ['messages'],
+    data: function () {
+        return {
+            message: null
+        }
+    },
     template:
-        '<div>' +
-            '<message-form :messages = "messages"/>' +
-            '<message-row v-for="message in messages" :key="message.id" :message = "message" />' +
+        '<div style="position: relative; width: 300px;">' +
+        '<message-form :messages = "messages" :messageAttr = "message"/>' +
+        '<message-row v-for="message in messages" :key="message.id" :message = "message" ' +
+            ':editMethod="editMethod" :messages="messages"/>' +
         '</div>',
     created: function () {
         messageApi.get().then(result =>
@@ -43,6 +94,11 @@ Vue.component('messages-list', {
                 data.forEach(message => this.messages.push(message))
             )
         )
+    },
+    methods: {
+        editMethod: function (message) {
+            this.message = message;
+        }
     }
 });
 
